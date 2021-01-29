3fc822f271b230973b2f037bc6041f27b7ef11a8
nicicvladan804 <57561653+nicicvladan804@users.noreply.github.com>
2021-01-29 01:18:48 +0100

    init

---
+++ js/javascript.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.partners').bxSlider({
+      slideWidth: 220,
+      minSlides: 2,
+      maxSlides: 6,
+      slideMargin: 25,
+      moveSlides: 1,
+      pager: false,
+      auto: true
+    });
+  });
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
