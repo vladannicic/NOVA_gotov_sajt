4ec089f4e277d9aaf6ddc1d1fc7998114b54fb57
nicicvladan804 <57561653+nicicvladan804@users.noreply.github.com>
2021-02-28 19:14:43 +0100

    commit3

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,5 @@
 
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });
+
   function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
